390cea4ac8bfc00e06994e0ec8e197871f3a673a
Steve Lawson <stevelawsonspi@gmail.com>
2018-10-26 09:41:16 +0000

    Adjust headings a bit

--- test/controllers/location_controller_test.rb
+++ test/controllers/location_controller_test.rb
@@ -4,7 +4,7 @@ class LocationControllerTest < ActionDispatch::IntegrationTest
   test "root should get index" do
     get root_url
     assert_response :success
-    assert_select "h1", "Locations"
+    assert_select "h3", "Select location:"
   end
 
 end
