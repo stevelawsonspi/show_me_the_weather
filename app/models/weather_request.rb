c8bc5870b9fa1c2d61bc3151b1c185152d7e32bb
Steve Lawson <stevelawsonspi@gmail.com>
2018-11-07 06:53:38 +0000

    Fixing issue caused by cloude IDE

--- app/models/weather_request.rb
+++ app/models/weather_request.rb
@@ -1,9 +1,11 @@
 class WeatherRequest < ApplicationRecord
   belongs_to :location
 
-  STATUSES = [PROCESSING = 'Processing',
-              ERROR      = 'Error',
-              SUCCESS    = 'Success'].freeze
+  STATUSES = [
+    PROCESSING = 'Processing',
+    ERROR      = 'Error',
+    SUCCESS    = 'Success'
+  ].freeze
 
   validates :status, inclusion: { in: STATUSES }
 
@@ -12,7 +14,7 @@ class WeatherRequest < ApplicationRecord
   end
   
   def error?
-    status = ERROR
+    status == ERROR
   end
   
   def weather_info
@@ -21,12 +23,14 @@ class WeatherRequest < ApplicationRecord
     wind      = hash_data.dig('query', 'results', 'channel', 'wind')
     astronomy = hash_data.dig('query', 'results', 'channel', 'astronomy')
     condition = hash_data.dig('query', 'results', 'channel', 'item', 'condition')
-    OpenStruct.new(weather_time: condition['date'],
-                   temperature:  Formulas.fahrenheit_to_celsius(condition['temp'].to_f),
-                   feels_like:   Formulas.fahrenheit_to_celsius(wind['chill'].to_f),
-                   description:  condition['text'],
-                   sunrise:      astronomy['sunrise'],
-                   sunset:       astronomy['sunset'])
+    OpenStruct.new(
+      weather_time: condition['date'],
+      temperature:  to_celsius(condition['temp'].to_f),
+      feels_like:   to_celsius(wind['chill'].to_f),
+      description:  condition['text'],
+      sunrise:      astronomy['sunrise'],
+      sunset:       astronomy['sunset']
+    )
   end
 
   def weather_forecasts
@@ -35,11 +39,13 @@ class WeatherRequest < ApplicationRecord
     forecasts = hash_data.dig('query', 'results', 'channel', 'item', 'forecast')
     forecast_array = []
     forecasts.each do |forecast|
-      forecast_array << OpenStruct.new(date:        forecast['date'],
-                                       day_name:    forecast['day'],
-                                       temp_high:   Formulas.fahrenheit_to_celsius(forecast['high'].to_f),
-                                       temp_low:    Formulas.fahrenheit_to_celsius(forecast['low'].to_f),
-                                       description: forecast['text'])
+      forecast_array << OpenStruct.new(
+        date:        forecast['date'],
+        day_name:    forecast['day'],
+        temp_high:   to_celsius(forecast['high'].to_f),
+        temp_low:    to_celsius(forecast['low'].to_f),
+        description: forecast['text']
+      )
     end
     forecast_array
   end
@@ -48,6 +54,10 @@ class WeatherRequest < ApplicationRecord
 
     def returned_json_as_hash
       @hash_data ||= JSON.parse(returned_json)
+    end
+
+    def to_celsius(fahrenheit)
+      Formulas.fahrenheit_to_celsius(fahrenheit)
     end
 
 end
