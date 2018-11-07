f1df93acfc53a06775466f9c5076229683714447
Steve Lawson <stevelawsonspi@gmail.com>
2018-11-07 06:21:32 +0000

    Moved calc for fahrenheit to celsius to a module

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module WeatherApp
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     
+    # auto-load from /lib
+    config.autoload_paths += %W(#{config.root}/lib)
     # Yahoo weather service
     config.yahoo_weather_url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3DLOCATION_ID&format=json"
   end
