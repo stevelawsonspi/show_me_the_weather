22584854d8cdd4ab92a9f2100bf44deb538f5f04
Steve Lawson <stevelawsonspi@gmail.com>
2018-12-02 05:57:27 +0000

    Locations CRUD (still ugly)

---
+++ app/views/locations/_location.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! location, :id, :id, :name, :yahoo_id, :created_at, :updated_at, :created_at, :updated_at
+json.url location_url(location, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'weather_app#index'
-  get '/:id', to: 'weather_app#show'
+  resources :locations
+  get '/:id', to: 'weather_app#show' # defined last so it doesn't interfere with other routes
 end
